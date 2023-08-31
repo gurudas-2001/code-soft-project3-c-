2351a6287bedc21b5ba6ac0bc148c19efd0f90b2
gurudas-2001 <99683775+gurudas-2001@users.noreply.github.com>
2023-09-01 02:26:51 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter an operation (+, -, *, /): ";
+    std::cin >> operation;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    double result;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                std::cout << "Error: Division by zero is not allowed.\n";
+                return 1; // Exit with an error code
+            }
+            break;
+        default:
+            std::cout << "Invalid operation.\n";
+            return 1; // Exit with an error code
+    }
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
